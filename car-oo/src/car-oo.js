89c19c1c4bf8aec6a03b1b0138802b32d79f8e6e
Almir Campos <almircampos@gmail.com>
2020-09-20 08:12:48 -0700

    [U11][CAR-OO] Part Three Done

--- car-oo/src/car-oo.js
+++ car-oo/src/car-oo.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.log('\n### VEHICLE ###')
+console.log('\n### PART ONE - VEHICLE ###')
 class Vehicle{
     constructor( make, model, year){
         this.make = make;
@@ -19,7 +19,7 @@ const vehicle = new Vehicle( "Cururu", "Trash", "55 BC");
 console.log( vehicle.honk() );
 console.log( vehicle.toString() );
 
-console.log('\n### CAR ###')
+console.log('\n### PART TWO - CAR ###')
 class Car extends Vehicle {
     constructor(make, model, year){
         super( make, model, year);
@@ -31,3 +31,20 @@ let myFirstCar = new Car( 'Toyota', 'Corolla', 2005 );
 console.log( myFirstCar.toString() );
 console.log( myFirstCar.honk() );
 console.log( myFirstCar.numWheels );
+
+console.log('\n### PART THREE - MOTORCYCLE ###')
+class Motorcycle extends Vehicle {
+    constructor( model, make, year ){
+        super( model, make, year );
+        this.numWheels = 2;
+    }
+    revEngine(){
+        return "VROOM!!!"
+    }
+}
+
+let {honk, numWheels, revEngine} = new Motorcycle('Honda', 'Nighthawk', 2000);
+console.log( honk() );
+console.log( revEngine() );
+console.log( numWheels );
+
