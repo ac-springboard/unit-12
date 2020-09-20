35175a892b33c7e7fec056644824abbce9118341
Almir Campos <almircampos@gmail.com>
2020-09-20 08:46:19 -0700

    [U11][CAR-OO] Part Four Done - Exercise Completed

--- car-oo/src/car-oo.js
+++ car-oo/src/car-oo.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log('\n### PART ONE - VEHICLE ###')
+
 class Vehicle{
     constructor( make, model, year){
         this.make = make;
@@ -20,6 +21,7 @@ console.log( vehicle.honk() );
 console.log( vehicle.toString() );
 
 console.log('\n### PART TWO - CAR ###')
+
 class Car extends Vehicle {
     constructor(make, model, year){
         super( make, model, year);
@@ -33,6 +35,7 @@ console.log( myFirstCar.honk() );
 console.log( myFirstCar.numWheels );
 
 console.log('\n### PART THREE - MOTORCYCLE ###')
+
 class Motorcycle extends Vehicle {
     constructor( model, make, year ){
         super( model, make, year );
@@ -48,3 +51,38 @@ console.log( honk() );
 console.log( revEngine() );
 console.log( numWheels );
 
+console.log('\n### PART FOUR - GARAGE ###')
+
+class Garage {
+    constructor( numVehicles ){
+        this.numVehicles = numVehicles;
+        this.vehicles = new Array( numVehicles );
+    }
+
+    add ( vehicle ){
+        const isVehicle = vehicle instanceof Vehicle;
+        if ( !isVehicle ){
+            console.log( 'Sorry, only vehicles are allowed in here!');
+            return;
+        }
+
+        if( this.numVehicles === 0 ){
+            console.log( "Sorry, we're full.");
+            return;
+        }
+
+        this.vehicles.push( vehicle );
+        this.numVehicles--;
+        console.log( `Vehicle added: ${vehicle}`);
+    }
+}
+
+// let { vehicles, add } = new Garage( 2 ); // This won't work. Why?
+const garage = new Garage( 2 );
+console.log( garage.vehicles);
+garage.add( new Car('Hyundai', 'Elantra', 2015 ));
+console.log( garage.vehicles);
+garage.add( 'Taco');
+garage.add( new Motorcycle('Honda', 'Nighthawk', 2000 ));
+console.log( garage.vehicles);
+garage.add( new Motorcycle('Honda', 'Nighthawk', 2001 ));
